77446df2fe08716fc8be009d3df7cd2148af590f
Filippo Rossi <rossifilippo2000@gmail.com>
2021-12-10 15:43:53 +0100

    add title

---
+++ src/components/Title.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'Title'
+    }
+</script>
+
+
+<style scoped>
+    h1{
+        background-color: #fff;
+        width: 600px;
+        text-align: center;
+        margin: 100px auto;
+        padding: 20px;
+        transform: rotate(-5deg);
+        border-top-left-radius: 30px;
+        border-bottom-right-radius: 30px;
+        border: 3px solid #333;
+    }
+</style>
